0abc83936d90b795da687c421d34d602abd728e4
Dmitry <svobodiannikov@gmail.com>
2024-02-03 01:04:16 +0300

    Revert "Fix wildberries parsing"
    This reverts commit 1a7b7939ae672aa12d8e52a47a6ff0d83fe19ef8.

--- app/parsers.py
+++ app/parsers.py
@@ -10,39 +10,6 @@ from loguru import logger
 from app.schemas import ItemInfoResponseSchema
 
 
-def _get_wb_basket(nm_id: int):
-    nm_id = nm_id // 100000
-    if nm_id >= 0 and nm_id <= 143:
-        return "01"
-    if nm_id >= 144 and nm_id <= 287:
-        return "02"
-    if nm_id >= 288 and nm_id <= 431:
-        return "03"
-    if nm_id >= 432 and nm_id <= 719:
-        return "04"
-    if nm_id >= 720 and nm_id <= 1007:
-        return "05"
-    if nm_id >= 1008 and nm_id <= 1061:
-        return "06"
-    if nm_id >= 1062 and nm_id <= 1115:
-        return "07"
-    if nm_id >= 1116 and nm_id <= 1169:
-        return "08"
-    if nm_id >= 1170 and nm_id <= 1313:
-        return "09"
-    if nm_id >= 1314 and nm_id <= 1601:
-        return "10"
-    if nm_id >= 1602 and nm_id <= 1655:
-        return "11"
-    if nm_id >= 1656 and nm_id <= 1919:
-        return "12"
-    if nm_id >= 1920 and nm_id <= 2045:
-        return "13"
-    if nm_id >= 2046 and nm_id <= 2189:
-        return "14"
-    return "15"
-
-
 def try_parse_item_by_link(
     link: str, html: str | None
 ) -> Optional[ItemInfoResponseSchema]:
@@ -75,11 +42,10 @@ def try_parse_item_by_link(
         match = re.search(r'catalog\/(\d+)', link)
         if not match:
             return None
-        item_id = int(match.group(1))
-        vol = item_id // 100000
-        part = item_id // 1000
-        basket = _get_wb_basket(item_id)
-        base_url = f'https://basket-{basket}.wbbasket.ru/vol{vol}/part{part}/{item_id}'
+        item_id = match.group(1)
+        vol = item_id[:3]
+        part = item_id[:5]
+        base_url = f'https://basket-02.wbbasket.ru/vol{vol}/part{part}/{item_id}'
         api_response = httpx.get(f'{base_url}/info/ru/card.json')
         api_response.raise_for_status()
         api_data = api_response.json()
