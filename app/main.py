9d5f3fab207914c6939dca00966324e7b94c834f
Dmitry <svobodiannikov@gmail.com>
2024-03-27 20:46:05 +0300

    Test exception

--- app/main.py
+++ app/main.py
@@ -782,6 +782,7 @@ async def get_item_info_from_page(
     request_data: ItemInfoRequestSchema,
     # user: User = Depends(get_current_user),  # TODO:
 ) -> ItemInfoResponseSchema:
+    raise Exception('test')
     try:
         result = await try_parse_item_by_link(str(request_data.link), request_data.html)
     except ItemInfoParseError as ex:
