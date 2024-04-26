5c7440ca45142880c2571a42368ccb036a6b1816
Dmitry <svobodiannikov@gmail.com>
2024-04-26 18:11:09 +0300

    Remove unused config

--- app/config.py
+++ app/config.py
@@ -7,7 +7,6 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     IS_DEBUG: bool
     VK_SERVICE_KEY: str
-    VK_PROTECTED_KEY: str
     VK_APP_ID: int
     FIREBASE_KEY_PATH: Path
     TEST_TOKEN: str
