358e7410c9c8e16409f627c46586ad1ce28a9968
Dmitry <svobodiannikov@gmail.com>
2024-10-20 13:52:18 +0300

    Отдельный лог-файл для эксепшенов

--- app/logging.py
+++ app/logging.py
@@ -3,4 +3,15 @@ from loguru import logger
 from app.config import settings
 
 log_level = 'DEBUG' if settings.IS_DEBUG else 'INFO'
-logger.add(settings.LOGS_DIR / 'log.log', level=log_level)
+logger.add(
+    settings.LOGS_DIR / 'log.log',
+    level=log_level,
+    backtrace=False,
+    diagnose=False,
+)
+logger.add(
+    settings.LOGS_DIR / 'errors.log',
+    level='ERROR',
+    backtrace=True,
+    diagnose=True,
+)
